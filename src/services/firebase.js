a535255020230f97454edd674e7598dcc5d3e622
Gustavo Benjamin <gustasbenjamin@gmail.com>
2023-11-01 19:56:08 -0300

    pastas e libs baixados/configurados

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBn846JYK7M6RrZ8Xyo0HNLb-GGwlKTqr4",
+  authDomain: "login-receitas-fe941.firebaseapp.com",
+  projectId: "login-receitas-fe941",
+  storageBucket: "login-receitas-fe941.appspot.com",
+  messagingSenderId: "446984963625",
+  appId: "1:446984963625:web:a264fc1bebba7908667e38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
